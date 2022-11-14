491d80845a38825c7a852ff2fd12a9dac6eac50f
hyunseo <goatsfish@gmail.com>
2022-11-14 13:00:46 +0900

    init : Spring AOP

---
+++ spring-aop/src/main/java/org/pro/springorder/voucher/VoucherService.java
@@ -0,0 +1,28 @@
+package org.pro.springorder.voucher;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.text.MessageFormat;
+import java.util.UUID;
+
+@Service
+public class VoucherService {
+
+    private final VoucherRepository voucherRepository;
+
+    @Autowired
+    public VoucherService(VoucherRepository voucherRepository) {
+        this.voucherRepository = voucherRepository;
+    }
+
+    public Voucher getVoucher(UUID voucherId) {
+        return voucherRepository
+                .findById(voucherId)
+                .orElseThrow(() -> new RuntimeException(MessageFormat.format("Can not find a voucher for {0}", voucherId)));
+    }
+
+    public void useVoucher(Voucher voucher) {
+
+    }
+}
