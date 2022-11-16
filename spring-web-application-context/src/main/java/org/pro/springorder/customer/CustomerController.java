3557f5273fcaee10b9e54a40036e13a1480b0d88
hyunseo <goatsfish@gmail.com>
2022-11-16 18:21:55 +0900

    init : spring-web-application-context

---
+++ spring-web-application-context/src/main/java/org/pro/springorder/customer/CustomerController.java
@@ -0,0 +1,52 @@
+package org.pro.springorder.customer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+@Controller
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    //    @RequestMapping(value = "/customers", method = RequestMethod.GET)
+    @GetMapping("/customers")
+    public String findCustomers(Model model) {
+        var allCustomers = customerService.getAllCustomers();
+        model.addAttribute("serverTime", LocalDateTime.now());
+        model.addAttribute("customers", allCustomers);
+        return "views/customers";
+    }
+
+    @GetMapping("/customers/{customerId}")
+    public String findCustomer(@PathVariable UUID customerId, Model model) {
+        var maybeCustomer = customerService.getCustomer(customerId);
+        if (maybeCustomer.isPresent()) {
+            model.addAttribute("customer", maybeCustomer.get());
+            return "views/customer-details";
+        } else {
+            return "views/404";
+        }
+    }
+
+    @GetMapping("/customers/new")
+    public String viewNewCustomerPage() {
+        return "views/new-customers";
+    }
+
+    @PostMapping("/customers/new")
+    public String addNewCustomer(CreateCustomerRequest createCustomerRequest) {
+        customerService.createCustomer(createCustomerRequest.email(), createCustomerRequest.name());
+        return "redirect:/customers";
+    }
+}
