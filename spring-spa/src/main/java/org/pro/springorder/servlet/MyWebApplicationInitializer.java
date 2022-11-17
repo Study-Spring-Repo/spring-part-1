c7820e3cad3be8cb21eac3eb595aadbd514a578c
hyunseo <goatsfish@gmail.com>
2022-11-18 07:27:51 +0900

    init : spring-spa

--- spring-spa/src/main/java/org/pro/springorder/servlet/MyWebApplicationInitializer.java
+++ spring-spa/src/main/java/org/pro/springorder/servlet/MyWebApplicationInitializer.java
@@ -1,6 +1,9 @@
 package org.pro.springorder.servlet;
 
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.zaxxer.hikari.HikariDataSource;
+import org.pro.springorder.customer.CustomerController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
@@ -10,12 +13,19 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.MarshallingHttpMessageConverter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.oxm.xstream.XStreamMarshaller;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -29,15 +39,21 @@ import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import javax.servlet.ServletContext;
 import javax.sql.DataSource;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class MyWebApplicationInitializer implements WebApplicationInitializer {
     private static final Logger logger = LoggerFactory.getLogger(MyWebApplicationInitializer.class);
 
     @Configuration
     @EnableWebMvc
-    @ComponentScan(basePackages = "org.pro.springorder.customer")
-    @EnableTransactionManagement
-    static class AppConfig implements WebMvcConfigurer, ApplicationContextAware {
+    @ComponentScan(basePackages = "org.pro.springorder.customer",
+            includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = CustomerController.class),
+            useDefaultFilters = false
+    )
+    static class ServletConfig implements WebMvcConfigurer, ApplicationContextAware {
 
         ApplicationContext applicationContext;
 
@@ -55,13 +71,13 @@ public class MyWebApplicationInitializer implements WebApplicationInitializer {
             var thymeleafViewResolver = new ThymeleafViewResolver();
             thymeleafViewResolver.setTemplateEngine(springTemplateEngine);
             thymeleafViewResolver.setOrder(1);
-            thymeleafViewResolver.setViewNames(new String[]{"views/*"});
+            thymeleafViewResolver.setViewNames(new String[]{"views/"});
             registry.viewResolver(thymeleafViewResolver);
         }
 
         @Override
         public void addResourceHandlers(ResourceHandlerRegistry registry) {
-            registry.addResourceHandler("/resources/**")
+            registry.addResourceHandler("/resources/*")
                     .addResourceLocations("/resources/")
                     .setCachePeriod(60)
                     .resourceChain(true)
@@ -100,18 +116,70 @@ public class MyWebApplicationInitializer implements WebApplicationInitializer {
         public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
             this.applicationContext = applicationContext;
         }
+
+        @Override
+        public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+            var messageConverter = new MarshallingHttpMessageConverter();
+            var xStreamMarshaller = new XStreamMarshaller();
+            messageConverter.setMarshaller(xStreamMarshaller);
+            messageConverter.setUnmarshaller(xStreamMarshaller);
+
+            var javaTimeModule = new JavaTimeModule();
+            javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ISO_DATE_TIME));
+            var modules = Jackson2ObjectMapperBuilder.json().modules(javaTimeModule);
+
+            converters.add(1, new MappingJackson2HttpMessageConverter(modules.build()));
+        }
     }
 
+    @Configuration
+    @ComponentScan(basePackages = "org.pro.springorder.customer",
+            excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = CustomerController.class)
+    )
+    @EnableTransactionManagement
+    static class RootConfig {
+        @Bean
+        public DataSource dataSource() {
+            var dataSource = DataSourceBuilder.create()
+                    .url("jdbc:mysql://localhost/order_mgmt")
+                    .username("root")
+                    .password("root1234!")
+                    .type(HikariDataSource.class)
+                    .build();
+            dataSource.setMaximumPoolSize(1000);
+            dataSource.setMinimumIdle(100);
+            return dataSource;
+        }
+
+        @Bean
+        public JdbcTemplate jdbcTemplate() {
+            return new JdbcTemplate(dataSource());
+        }
+
+        @Bean
+        public NamedParameterJdbcTemplate namedParameterJdbcTemplate(JdbcTemplate jdbcTemplate) {
+            return new NamedParameterJdbcTemplate(jdbcTemplate);
+        }
+
+        @Bean
+        public PlatformTransactionManager platformTransactionManager(DataSource dataSource) {
+            return new DataSourceTransactionManager(dataSource);
+        }
+    }
     @Override
     public void onStartup(ServletContext servletContext) {
-        var applicationContext = new AnnotationConfigWebApplicationContext();
-        applicationContext.register(AppConfig.class);
+        var rootApplicationContext = new AnnotationConfigWebApplicationContext();
+        rootApplicationContext.register(RootConfig.class);
+        var loaderListener = new ContextLoaderListener(rootApplicationContext);
+        servletContext.addListener(loaderListener);
 
-//        var dispatcherServlet = new DispatcherServlet(applicationContext);
+        var applicationContext = new AnnotationConfigWebApplicationContext();
+        applicationContext.register(ServletConfig.class);
+        var dispatcherServlet = new DispatcherServlet(rootApplicationContext);
         logger.info("Starting Server...");
 //        var servletRegistration = servletContext.addServlet("test", dispatcherServlet);
 //        servletRegistration.addMapping("/");
-//        servletRegistration.setLoadOnStartup(1);
+//        servletRegistration.setLoadOnStartup(0);
     }
 
 }
