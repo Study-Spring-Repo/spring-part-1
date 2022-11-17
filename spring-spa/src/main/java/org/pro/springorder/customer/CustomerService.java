92177b1add1cfb7b4f92c8b5b8d188b0cae9cddd
hyunseo <goatsfish@gmail.com>
2022-11-18 07:26:12 +0900

    init : spring-spa

---
+++ spring-spa/src/main/java/org/pro/springorder/customer/CustomerService.java
@@ -0,0 +1,10 @@
+package org.pro.springorder.customer;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    void createCustomers(List<Customer> customers);
+
+    List<Customer> getAllCustomers();
+}
